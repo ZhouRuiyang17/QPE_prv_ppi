dc6b341436f454c915089e6518ce09a10281bdcb
Zhou Ruiyang <m18088042911@163.com>
2023-12-26 20:09:03 +0800

    update

--- 数据集分析.py
+++ 数据集分析.py
@@ -21,39 +21,80 @@ import datetime
 # df.to_csv(r"D:\data\zdr_zhangzheQC_dataset.csv")
 # =============================================================================
 
-prv = pd.read_csv(r"D:\data\kdp_zhangzheQC_dataset.csv", index_col = 0).fillna(-999)
-prv.index = pd.to_datetime(prv.index)
-prv[prv<0] = 0
+'''1h'''
+# =============================================================================
+# prv = pd.read_csv(r"D:\data\ref_zhangzheQC_dataset.csv", index_col = 0).fillna(-999)
+# prv.index = pd.to_datetime(prv.index)
+# prv[prv<0] = 0
+# # prv = 10**(prv/10)
+# # a,b = 0.03468, 0.5869
+# # a,b = 14.93, 0.83
+# # rainrate = a*prv**b
+# zdr = pd.read_csv(r"D:\data\zdr_zhangzheQC_dataset.csv", index_col = 0).fillna(-999)
+# zdr.index = pd.to_datetime(zdr.index)
+# zdr[zdr<0] = 0
+# zdr = 10**(zdr/10)
+# # a,b,c = 0.00614, 0.959, -3.671
+# a,b,c = 22.56, 0.91, -0.859
+# rainrate = a*prv**b*zdr**c
+# rainrate[rainrate < 0.1] = 0
+# acc3min = rainrate * 3/60
+# acc1h = acc3min.resample('H', label = 'right').sum()
+# 
+# gauge = pd.read_csv(r"D:\data\城市院自动站_all.csv", index_col = 0).fillna(0)
+# gauge.index = pd.to_datetime(gauge.index)
+# gauge[gauge<0] = 0
+# gauge[gauge>1000] = 0
+# 
+# index_radar = acc1h.index.to_frame()
+# index_gauge = gauge.index.to_frame()
+# index_common = pd.merge(index_gauge, index_radar)
+# columns = acc1h.columns
+# acc1h = acc1h.loc[index_common[0], columns]
+# gauge = gauge.loc[index_common[0], columns]
+# 
+# xxx = gauge.values.flatten()
+# yyy = acc1h.values.flatten()
+# loc = (xxx>0) & (yyy>0)
+# scatter = mt.Scatter(xxx[loc], yyy[loc])
+# scatter.plot3(bins = [np.arange(0, 50, 0.5)]*2, lim=[[0,50]]*2, show_metrics=1, draw_line=1)
+# =============================================================================
+
+'''RR'''
+# prv = pd.read_csv(r"D:\data\ref_zhangzheQC_dataset.csv", index_col = 0).fillna(-999)
+# prv.index = pd.to_datetime(prv.index)
+# prv[prv<0] = 0
 # prv = 10**(prv/10)
 # a,b = 0.03468, 0.5869
-# a,b = 14.93, 0.83
+# # a,b = 14.93, 0.83
 # rainrate = a*prv**b
-zdr = pd.read_csv(r"D:\data\zdr_zhangzheQC_dataset.csv", index_col = 0).fillna(-999)
-zdr.index = pd.to_datetime(zdr.index)
-zdr[zdr<0] = 0
-zdr = 10**(zdr/10)
-# a,b,c = 0.00614, 0.959, -3.671
-a,b,c = 22.56, 0.91, -0.859
-rainrate = a*prv**b*zdr**c
-rainrate[rainrate < 0.1] = 0
-acc3min = rainrate * 3/60
-acc1h = acc3min.resample('H', label = 'right').sum()
+# # =============================================================================
+# # zdr = pd.read_csv(r"D:\data\zdr_zhangzheQC_dataset.csv", index_col = 0).fillna(-999)
+# # zdr.index = pd.to_datetime(zdr.index)
+# # zdr[zdr<0] = 0
+# # zdr = 10**(zdr/10)
+# # # a,b,c = 0.00614, 0.959, -3.671
+# # a,b,c = 22.56, 0.91, -0.859
+# # rainrate = a*prv**b*zdr**c
+# # =============================================================================
+# rainrate[rainrate < 0.1] = 0
 
-gauge = pd.read_csv(r"D:\data\城市院自动站_all.csv", index_col = 0).fillna(0)
-gauge.index = pd.to_datetime(gauge.index)
-gauge[gauge<0] = 0
-gauge[gauge>1000] = 0
 
-index_radar = acc1h.index.to_frame()
-index_gauge = gauge.index.to_frame()
-index_common = pd.merge(index_gauge, index_radar)
-columns = acc1h.columns
-acc1h = acc1h.loc[index_common[0], columns]
-gauge = gauge.loc[index_common[0], columns]
+# gauge = pd.read_csv(r"D:\data\RR_all.csv", index_col = 0).fillna(0)
+# gauge.index = pd.to_datetime(gauge.index)
+# gauge[gauge<0] = 0
+# gauge[gauge>1000] = 0
+# gauge = gauge.resample('3min', label='left').mean()
 
-xxx = gauge.values.flatten()
-yyy = acc1h.values.flatten()
-loc = (xxx>0) & (yyy>0)
-scatter = mt.Scatter(xxx[loc], yyy[loc])
-scatter.plot3(bins = [np.arange(0, 50, 0.5)]*2, lim=[[0,50]]*2, show_metrics=1, draw_line=1)
+# index_radar = rainrate.index.to_frame()
+# index_gauge = gauge.index.to_frame()
+# index_common = pd.merge(index_gauge, index_radar)
+# columns = rainrate.columns
+# rainrate = rainrate.loc[index_common[0], columns]
+# gauge = gauge.loc[index_common[0], columns]
 
+# xxx = gauge.values.flatten()
+# yyy = rainrate.values.flatten()
+# loc = (xxx>0) & (yyy>0)
+# scatter = mt.Scatter(xxx[loc], yyy[loc])
+# scatter.plot3(bins = [np.arange(0, 50, 0.5)]*2, lim=[[0,50]]*2, show_metrics=1, draw_line=1)
