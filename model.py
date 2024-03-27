18afe258f5d4ac332e60aae24d084c186ee67a20
Zhou Ruiyang <m18088042911@163.com>
2024-03-27 10:39:35 +0800

    rewrite cnn_tian

--- model.py
+++ model.py
@@ -10,21 +10,36 @@ import torch.nn as nn
 class CNN_tian(nn.Module):
     def __init__(self):
         super(CNN_tian, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=0)
-        self.flt = nn.Flatten()
-        self.fc3 = nn.Linear(32*7*7, 1024)
-        self.fc5 = nn.Linear(1024, 256)
-        self.fc6 = nn.Linear(256, 1)
-
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+            )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=0),
+            nn.ReLU()
+            )
+        self.flt = nn.Sequential(
+            nn.Flatten()
+            )
+        self.fc1 = nn.Sequential(
+            nn.Linear(32*7*7, 1024),
+            nn.ReLU()
+            )
+        self.fc2 = nn.Sequential(
+            nn.Linear(1024, 256),
+            nn.ReLU()
+            )
+        self.out = nn.Sequential(
+            nn.Linear(256, 1),
+            )
 
     def forward(self, x):        
-        x = nn.ReLU(self.conv1(x))
-        x = nn.ReLU(self.conv2(x))
-        x = nn.Flatten(x)
-        x = nn.ReLU(self.fc3(x))
-        x = nn.ReLU(self.fc5(x))
-        x = self.fc6(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.flt(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.out(x)
         return x
 
 class CNN(nn.Module):
