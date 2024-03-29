e3a09e47738ab9e184bbd1feed0d532c4c04a197
Zhou Ruiyang <m18088042911@163.com>
2024-03-29 11:39:28 +0800

    Update apply.py

--- apply.py
+++ apply.py
@@ -8,17 +8,42 @@ from model import *
 import utils
 
 
-
-if __name__ == "__main__":
+def lookup(scan):
+    _, num_gate = scan[0][0].shape
+    left = scan[:, :, -4:, :]
+    right = scan[:, :, :4, :]
+    scan = np.append(left, scan, axis = 2)
+    scan = np.append(scan, right, axis= 2)
     
+    container = np.zeros((6,9,9))*1.
+    features = []; azis = []; gates = []
+    for azi in range(360):
+        pst = azi + 4
+        for gate in range(0+4, 1000-4):
+            container[0:2] = scan[0,:,pst-4:pst+5, gate-4:gate+5]
+            container[2:4] = scan[1,:,pst-4:pst+5, gate-4:gate+5]
+            container[4:6] = scan[3,:,pst-4:pst+5, gate-4:gate+5]
 
-    # 检查 GPU 是否可用
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("使用的设备:", device)
+            # ref = container[1]; refup = 10**(ref/10)
+            # meanup = np.mean(refup); mean = 10*np.log10(meanup)
+            mean1 = 10*np.log10(np.mean(10**(container[0]/10)))
+            mean2 = 10*np.log10(np.mean(10**(container[1]/10)))
+            if mean2 > 15 or mean1 > 15:
+                features += [container.copy()]
+                azis += [azi]
+                gates += [gate]
+    features = np.array(features).astype(np.float32)
+    azis = np.array(azis)
+    gates = np.array(gates)
+    azi_gate = np.vstack([azis, gates]).transpose()
     
+    return features, azis, gates
+
+def QPE(features):
     # ----数据归一并tensor化
-    features = []
-    features = utils.scaler(features, 'ref')
+    features[:,0:2] = utils.scaler(features[:,0:2], 'ref')
+    features[:,2:4] = utils.scaler(features[:,2:4], 'zdr')
+    features[:,4:6] = utils.scaler(features[:,4:6], 'kdp')
     features = torch.from_numpy(features)
 
     # ----加载模型并指定设备
@@ -33,4 +58,38 @@ if __name__ == "__main__":
     # ----结果numpy化并逆归一
     pred = pred.view(-1).detach().numpy()
     pred = utils.scaler(pred, 'rr', 1)
+
+    return pred
+
+def main(fpath):
+    # import my.mytools as mt
+    # info = mt.BJXSY
+    scan = np.load(fpath)
+    features, azis, gates = lookup(scan)
+    pred = QPE(features)
+    
+    rr = np.zeros((360,1000))*1.
+    rr[azis, gates] = pred
+    # mt.RADAR(rr, 'rr', *info, eles=[1.45]).ppi(0, [75, 15])
+    # import my.radarsys as rds
+    # rd = rds.radar(scan[0],scan[1],scan[2],scan[3],scan[4])
+    # rd.qpe_mayu(1)
+    # mt.RADAR(rd.rr, 'rr', *info, eles=[1.45]).ppi(0, [75, 15])
+    fps = fpath.replace('prv','rr')
+    print(f"QPE: {fps}")
+    np.save(fps, rr)
+
+if __name__ == "__main__":
+    
+
+    # 检查 GPU 是否可用
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("使用的设备:", device)
+    
+    ls = []
+    for root, dirs, files in os.walk(r'D:\data\beijing\radar\测试radarsys\for_cnn_test-20240328'):
+        for file in files:
+            if '2019' in file and 'prv' in file:
+                ls += [os.path.join(root, file)]
+                main(os.path.join(root, file))
 
