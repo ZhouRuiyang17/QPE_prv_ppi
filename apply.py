4ccf48be01cc8dd63d7b0cd315f8f4341a1ef7be
Zhou Ruiyang <m18088042911@163.com>
2024-03-30 12:29:03 +0800

    Update apply.py

--- apply.py
+++ apply.py
@@ -26,18 +26,18 @@ def lookup(scan):
 
             # ref = container[1]; refup = 10**(ref/10)
             # meanup = np.mean(refup); mean = 10*np.log10(meanup)
-            mean1 = 10*np.log10(np.mean(10**(container[0]/10)))
-            mean2 = 10*np.log10(np.mean(10**(container[1]/10)))
-            if mean2 > 15 or mean1 > 15:
-                features += [container.copy()]
-                azis += [azi]
-                gates += [gate]
+            # mean1 = 10*np.log10(np.mean(10**(container[0]/10)))
+            # mean2 = 10*np.log10(np.mean(10**(container[1]/10)))
+            # if container[1,4,4] > 0 or container[0,4,4] > 0:
+            features += [container.copy()]
+            azis += [azi]
+            gates += [gate]
     features = np.array(features).astype(np.float32)
     azis = np.array(azis)
     gates = np.array(gates)
     azi_gate = np.vstack([azis, gates]).transpose()
     
-    return features, azis, gates
+    return features, azi_gate
 
 def QPE(features):
     # ----数据归一并tensor化
@@ -62,29 +62,37 @@ def QPE(features):
     return pred
 
 def main(fpath):
-    fps = fpath.replace('prv','rr')
-    if os.path.exists(fps):
-        return 1
+    # fps = fpath.replace('prv','rr')
+    # if os.path.exists(fps):
+    #     return 1
     
-    # import my.mytools as mt
-    # info = mt.BJXSY
+    import datetime
     scan = np.load(fpath)
-    features, azis, gates = lookup(scan)
+    t1 = datetime.datetime.now()
+    features, azi_gate = lookup(scan)
+    t2 = datetime.datetime.now()
     rr = np.zeros((360,1000))*1.
-    if features.size > 0:
-        pred = QPE(features)
-        rr[azis, gates] = pred
-        print('CNN-QPE')
+    # if features.size > 0:
+    #     pred = QPE(features)
+    #     rr[azi_gate[:,0], azi_gate[:,1]] = pred
+    #     print('CNN-QPE')
+    t3 = datetime.datetime.now()
+    print(t2-t1, t3-t2)
     
+    import my.mytools as mt
+    info = mt.BJXSY
     # mt.RADAR(rr, 'rr', *info, eles=[1.45]).ppi(0, [75, 15])
-    # import my.radarsys as rds
-    # rd = rds.radar(scan[0],scan[1],scan[2],scan[3],scan[4])
+    import my.radarsys as rds
+    rd = rds.radar(scan[0],scan[1],scan[2],scan[3],scan[4])
     # rd.qpe_mayu(1)
-    # mt.RADAR(rd.rr, 'rr', *info, eles=[1.45]).ppi(0, [75, 15])
+    mt.RADAR(rd.phidp[0], 'phidp2', *info, eles=[0.5]).ppi(0, [75, 15])
+    mt.RADAR(rd.phidp[1], 'phidp2', *info, eles=[1.45]).ppi(0, [75, 15])
 
-    print(f"QPE: {fps}")
-    np.save(fps, rr)
-
+    # print(f"QPE: {fps}")
+    # np.save(fps, rr)
+    
+    # return features, azi_gate, pred
+    
 if __name__ == "__main__":
     
 
@@ -92,11 +100,13 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("使用的设备:", device)
     
-    ls = []
-    for root, dirs, files in os.walk(r'D:\data\beijing\radar\测试radarsys\for_cnn_test-20240328'):
-        for file in files:
-            if '2019' in file and 'prv' in file:
-                ls += [os.path.join(root, file)]
-                main(os.path.join(root, file))
+# =============================================================================
+#     ls = []
+#     for root, dirs, files in os.walk(r'D:\data\beijing\radar\测试radarsys\for_cnn_test-20240328'):
+#         for file in files:
+#             if '2019' in file and 'prv' in file:
+#                 ls += [os.path.join(root, file)]
+#                 main(os.path.join(root, file))
+# =============================================================================
                 
-    # main(r'D:\data\beijing\radar\测试radarsys\for_cnn_test-20240328\BJXSY.20190526.073711.prv.npy')
+    main(r'D:\data\beijing\radar\测试radarsys\for_cnn_test-20240328\BJXSY.20190526.024133.prv.npy')
