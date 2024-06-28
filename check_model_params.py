0b6bdea3d41c483ec961ed4caae3f7b3a0ad2fdb
zry <m18088042911@163.com>
2024-06-28 10:30:47 +0800

    tidyup

--- check_model_params.py
+++
@@ -1,9 +0,0 @@
-import torch
-from model import *
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("使用的设备:", device)
-model = CNN2()
-model.load_state_dict(torch.load("model/20240327-19-cnn_bn/cnn.pth"))
-for name, param in model.named_parameters():  
-    print(name, param.size())
