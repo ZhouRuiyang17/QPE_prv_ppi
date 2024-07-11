203684fa74b28b8b30bd58488f99d53666b4c37d
zry <m18088042911@163.com>
2024-07-11 15:11:15 +0800

    right res

---
+++ test-20240711ver.py
@@ -0,0 +1,139 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import torch
+
+from model import *
+import utils
+import my.mytools as mt
+
+import datetime
+
+path_save = './model/based_on_202407/{}'.format('240711-cnn-3prv-05per20-mseloss')
+
+# 检查 GPU 是否可用
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("使用的设备:", device)
+
+# input(f'res will be stored in:\n{path_save}\nshall we go on[y/n]?')
+
+
+def qpe(data, center):
+    ref = data[:,0, center, center]
+    zdr = data[:,1, center, center]
+    kdp = data[:,2, center, center]
+    refup = 10**(ref*0)
+    zdrup = 10**(zdr*0)
+
+    a1 = 0.0576; b1 = 0.557
+    a2 = 15.421; b2 = 0.817
+    a3 = 0.0059; b3 = 0.994;c3 = -4.929
+    a4 = 26.778; b4 = 0.946;c4 = -1.249
+
+
+
+    rr1 = a1*refup**b1
+    rr2 = a2*kdp**b2
+    rr3 = a3*refup**b3*zdrup**c3
+    rr4 = a4*kdp**b4*zdrup**c4
+
+    return rr1, rr2, rr3, rr4
+
+def apply(data):
+    refup = 10**(data[:, 0]*0)
+    meanup = refup.mean(axis=(1,2))
+    mean = 10*np.log10(meanup)
+    loc = mean >= 15
+    test_x = data[loc].astype(np.float32)
+
+    test_x[:,0] = utils.scaler(test_x[:,0], 'ref').astype(np.float32)
+    test_x[:,1] = utils.scaler(test_x[:,1], 'zdr').astype(np.float32)
+    test_x[:,2] = utils.scaler(test_x[:,2], 'kdp').astype(np.float32)
+    test_x = torch.from_numpy(test_x)
+
+    model = CNN_3prv()
+    model.load_state_dict(torch.load(path_save + '/' + "cnn.pth"))#,map_location=torch.device('cpu')))
+    model.eval()
+    with torch.no_grad():
+        pred = model(test_x)
+    
+    pred = pred.view(-1).detach().numpy()
+    pred = utils.scaler(pred, 'rr', 1)
+
+    rainrate = np.zeros(len(data))
+    rainrate[loc] = pred
+    return rainrate
+
+if __name__ == "__main__":
+    
+    # count = 0
+    # for file in os.listdir('../'):
+    #     if file.endswith('npz') and '2019' in file:
+    #         print(file)
+    #         f = np.load(f'../{file}')
+    #         data = f['data'][:, [3,4,5], 30-4:30+4+1, 30-4:30+4+1]
+    #         ts = [datetime.datetime.strptime(ts, '%Y%m%d%H%M') for ts in f['ts']]
+
+    #         stnm = file[5:10]
+    #         if count == 0:
+    #             radar = pd.DataFrame(index=ts)
+    #             radar1 = radar.copy()
+    #             radar2 = radar.copy()
+    #             radar3 = radar.copy()
+    #             radar4 = radar.copy()
+
+    #             radar.loc[ts, stnm] = apply(data)
+    #             rr1, rr2, rr3, rr4 = qpe(data, 4)
+    #             radar1.loc[ts, stnm] = rr1
+    #             radar2.loc[ts, stnm] = rr2
+    #             radar3.loc[ts, stnm] = rr3
+    #             radar4.loc[ts, stnm] = rr4
+    #             count += 1
+    #         else:
+    #             radar.loc[ts, stnm] = apply(data)
+    #             rr1, rr2, rr3, rr4 = qpe(data, 4)
+    #             radar1.loc[ts, stnm] = rr1
+    #             radar2.loc[ts, stnm] = rr2
+    #             radar3.loc[ts, stnm] = rr3
+    #             radar4.loc[ts, stnm] = rr4
+    #             count += 1
+    #         # break
+    # radar.to_csv(f'{path_save}/rainrate-dl.csv')
+    # radar1.to_csv(f'{path_save}/rainrate-ref.csv')
+    # radar2.to_csv(f'{path_save}/rainrate-kdp.csv')
+    # radar3.to_csv(f'{path_save}/rainrate-refzdr.csv')
+    # radar4.to_csv(f'{path_save}/rainrate-kdpzdr.csv')
+
+    radar = mt.readcsv(f'{path_save}/rainrate-dl.csv', isrr=3, mask=1, acc='H')
+    radar1 = mt.readcsv(f'{path_save}/rainrate-ref.csv', isrr=3, mask=1, acc='H')
+    radar2 = mt.readcsv(f'{path_save}/rainrate-kdp.csv', isrr=3, mask=1, acc='H')
+    radar3 = mt.readcsv(f'{path_save}/rainrate-refzdr.csv', isrr=3, mask=1, acc='H')
+    radar4 = mt.readcsv(f'{path_save}/rainrate-kdpzdr.csv', isrr=3, mask=1, acc='H')
+    gauge = mt.readcsv('../gauge_all.csv')
+
+    _, _, idx, col = mt.match_df(gauge, radar)
+    radar = radar.loc[idx, col]
+    radar1 = radar1.loc[idx, col]
+    radar2 = radar2.loc[idx, col]
+    radar3 = radar3.loc[idx, col]
+    radar4 = radar4.loc[idx, col]
+    gauge = gauge.loc[idx, col]
+
+    zzz = gauge.values
+    aaa = radar.values
+    bbb = radar1.values
+    ccc = radar2.values
+    ddd = radar3.values
+    eee = radar4.values
+    loc = (zzz>=0) & (aaa>=0) & (bbb>=0) & (ccc>=0) & (ddd>=0) & (eee>=0)
+    mt.Scatter(zzz[loc], aaa[loc]).plot3(bins=[np.arange(60)]*2, labels=['gauge (mm)', 'radar (mm)'], lim=[[0,60]]*2, show_metrics=1, draw_line=1,
+                                          fpath=f'{path_save}/eval-dl.png', title='dl')
+    mt.Scatter(zzz[loc], bbb[loc]).plot3(bins=[np.arange(60)]*2, labels=['gauge (mm)', 'radar (mm)'], lim=[[0,60]]*2, show_metrics=1, draw_line=1,
+                                        fpath=f'{path_save}/eval-ref.png', title='ref')
+    mt.Scatter(zzz[loc], ccc[loc]).plot3(bins=[np.arange(60)]*2, labels=['gauge (mm)', 'radar (mm)'], lim=[[0,60]]*2, show_metrics=1, draw_line=1,
+                                        fpath=f'{path_save}/eval-kdp.png', title='kdp')
+    mt.Scatter(zzz[loc], ddd[loc]).plot3(bins=[np.arange(60)]*2, labels=['gauge (mm)', 'radar (mm)'], lim=[[0,60]]*2, show_metrics=1, draw_line=1,
+                                        fpath=f'{path_save}/eval-refzdr.png', title='refzdr')
+    mt.Scatter(zzz[loc], eee[loc]).plot3(bins=[np.arange(60)]*2, labels=['gauge (mm)', 'radar (mm)'], lim=[[0,60]]*2, show_metrics=1, draw_line=1,
+                                        fpath=f'{path_save}/eval-kdpzdr.png', title='kdpzdr')
---
+++ train-20240711ver.py
@@ -0,0 +1,155 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import torch
+
+from model import *
+import utils
+
+path_save = './model/based_on_202407/{}'.format('240711-cnn-3prv-05per20-mseloss')
+if not os.path.exists(path_save):
+    os.makedirs(path_save)
+
+# 检查 GPU 是否可用
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("使用的设备:", device)
+
+# input(f'res will be stored in:\n{path_save}\nshall we go on[y/n]?')
+
+def plot(res1, res2, loss_train, loss_vali):
+    t = len(loss_train)
+
+    plt.cla()
+    fig = plt.figure(figsize=(6,6)); ax = fig.add_subplot()
+    rainrate = np.array(res1[1]).flatten(); prediction = np.array(res1[2]).flatten()
+    ax.hist2d(rainrate, prediction,bins = [np.arange(0,1,0.01)]*2, norm = colors.LogNorm())
+    ax.plot([0,1],[0,1])
+    ax.set_title('train')
+    ax.set_aspect('equal')
+    bias = np.sum(prediction - rainrate) / np.sum(rainrate)
+    ax.text(0.8,0.9,bias)
+    plt.savefig(path_save + '/train_epoch{}.png'.format(t), bbox_inches = 'tight')
+    plt.close()
+
+    plt.cla()
+    fig = plt.figure(figsize=(6,6)); ax = fig.add_subplot()
+    rainrate = np.array(res2[1]).flatten(); prediction = np.array(res2[2]).flatten()
+    ax.hist2d(rainrate, prediction,bins = [np.arange(0,1,0.01)]*2, norm = colors.LogNorm())
+    ax.plot([0,1],[0,1])
+    ax.set_title('vali')
+    ax.set_aspect('equal')
+    bias = np.sum(prediction - rainrate) / np.sum(rainrate)
+    ax.text(0.8,0.9,bias)
+    plt.savefig(path_save + '/vali_epoch{}.png'.format(t), bbox_inches = 'tight')
+    plt.close()
+
+    plt.cla()
+    fig = plt.figure(figsize=(6,6)); ax = fig.add_subplot()
+    ax.plot(loss_train, label = 'train loss')
+    ax.plot(loss_vali, label = 'vali loss')
+    ax.set_aspect('auto')
+    plt.legend()
+    plt.savefig(path_save + '/loss.png', bbox_inches = 'tight')
+    plt.close()
+
+if __name__ == "__main__":
+    
+    # ----封装
+    dataset_train = np.load('../dataset-1-9.npz')
+    train_x = dataset_train['x_train'].astype(np.float32)
+    train_y = dataset_train['y_train'].astype(np.float32)
+    vali_x = dataset_train['x_vali'].astype(np.float32)
+    vali_y = dataset_train['y_vali'].astype(np.float32)
+    print('Data loaded')
+
+    '''裁剪数据和归一化'''
+    train_x[:,0] = utils.scaler(train_x[:,0], 'ref')
+    vali_x[:,0] = utils.scaler(vali_x[:,0], 'ref')
+    train_x[:,1] = utils.scaler(train_x[:,1], 'zdr')
+    vali_x[:,1] = utils.scaler(vali_x[:,1], 'zdr')
+    train_x[:,2] = utils.scaler(train_x[:,2], 'kdp')
+    vali_x[:,2] = utils.scaler(vali_x[:,2], 'kdp')
+
+    train_y = train_y[:, 0].reshape(-1, 1)
+    vali_y = vali_y[:, 0].reshape(-1, 1)
+    train_y = utils.scaler(train_y, 'rr')
+    vali_y = utils.scaler(vali_y, 'rr')
+
+    '''数据加载'''
+    train = utils.loader(train_x, train_y, device, 64)
+    vali = utils.loader(vali_x, vali_y, device, 64)
+
+    
+    '''训练'''
+    model = CNN_3prv().to(device)
+    optimizer = torch.optim.Adam(model.parameters(),lr = 1e-4, weight_decay = 1e-4)
+    loss_func = torch.nn.MSELoss()
+    # loss_func = wmaeloss(weights, edge)
+    from torch.optim.lr_scheduler import StepLR
+    scheduler = StepLR(optimizer, step_size=20, gamma=0.5)
+
+
+    epochs = 500
+    loss_train = []; loss_vali = []
+    params = []; slopes = []; positive_counter = 0; positive_position = []
+    for t in range(epochs):
+        print(f"-------------------------------\nEpoch {t+1}")
+        
+        res1, res2 = utils.trainer(train, vali, model, loss_func, optimizer)
+        scheduler.step()
+        print(f"Epoch {t + 1}, Learning Rate:")
+        for param_group in optimizer.param_groups:
+            print(param_group['lr']) # 打印更新后的学习率
+        
+        loss_train += [res1[-1]]
+        loss_vali += [res2[-1]]
+        if t % 10 == 0 or t == 5:           
+            plot(res1, res2, loss_train, loss_vali)
+        
+        '''
+        always store the LAST epochs/10 of params and save the LAST params
+        check the slope of the loss_vali of the LAST epochs/10 
+        when slope > 0, count, and record the SLOPE and POSITION
+        when counter == 20, stop
+        '''
+        if len(params) < epochs/10:
+            params.append(model.state_dict())
+        else:
+            params.append(model.state_dict())
+            params = params[1:]
+            flag, slope = utils.early_stop(loss_vali, int(epochs/10))
+            torch.save(params[-1], path_save + '/' + "cnn.pth")
+
+            if flag and t >= 50:
+                slopes += [slope]
+                positive_position += [t]
+                positive_counter += flag
+            if positive_counter == 50:
+                torch.save(params[-1], path_save + '/' + "cnn.pth")
+                print('early stop at epoch:{}'.format(t))
+                plot(res1, res2, loss_train, loss_vali)
+                print(slopes)
+                print(positive_position)
+                break
+
+            # flag_stop = utils.early_stop_ptrend(loss_vali, 10)
+            # if flag_stop:
+            #     torch.save(params[-1], path_save + '/' + "cnn.pth")
+            #     print('early stop at epoch:{}'.format(t))
+            #     plot(res1, res2, loss_train, loss_vali)
+            #     break
+    
+    print("Done!")
+
+    
+    
+    if positive_counter != 20:
+        torch.save(model.state_dict(), path_save + '/' + "cnn.pth")
+        print('finish all epochs:{}'.format(epochs))  
+
+    loss_arr = np.array([loss_train, loss_vali]).T
+    np.savetxt(f'{path_save}/loss.csv', loss_arr, delimiter=',')
+
+
+    
