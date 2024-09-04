a843d4290e85f34667601e1ba99233a719786654
zry <m18088042911@163.com>
2024-09-04 10:41:56 +0800

    save changes

--- eval-p2r.py
+++ eval-p2r.py
@@ -1,11 +1,13 @@
 import sys
 sys.path.append('my')
 import my.utils.point2radar as p2r
-
+import os
 
 siteinfo_path = '/home/zry/code/gauge2bjxsy_145.csv'
-path = '/data/zry/radar/Xradar_npy_qpe/BJXSY'
-savepath = '/home/zry/code/QPE_prv_ppi/model/based_on_202407/240727-cnn-9prv-3out-wmse/'
+path = '/data/zry/radar/Xradar_npy_qpe/BJXSY-test/ResQPE-3399-1-vlr-wmse'
+savepath = '/home/zry/code/QPE_prv_ppi/model/based_on_202407/ResQPE-3399-1-vlr-wmse/eval'
+if not os.path.exists(savepath):
+    os.makedirs(savepath)
 
 df = p2r.lookup_site2radar(siteinfo_path, path, temp_var=0)
 df.to_csv(f'{savepath}/example-ref.csv')
