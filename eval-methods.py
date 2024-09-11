dc3d1ea42689df1bafaf69b992640e885ea7adc2
zry <m18088042911@163.com>
2024-09-11 15:17:02 +0800

    total and cases eval

--- eval-methods.py
+++ eval-methods.py
@@ -8,89 +8,121 @@ import matplotlib.colors as colors
 # plt.rcParams['font.family'] = 'Microsoft YaHei'
 plt.rcParams['font.size'] = 12
 
-path = '/home/zry/code/QPE_prv_ppi/model/based_on_202407/ResQPE-3399-1-vlr-wmse/eval'
+path = '/home/zry/code/QPE_prv_ppi/dataset/20240909eval_2models'
 
 
-aaa = mt.readcsv(f"{path}/example-dl.csv", 0, isrr=3, mask=1, acc='H')
-bbb = mt.readcsv(f"{path}/example-ref.csv", 0, isrr=3, mask=1, acc='H')
-ccc = mt.readcsv(f"{path}/example-kdp.csv", 0, isrr=3, mask=1, acc='H')
-# ddd = mt.readcsv(f"{path}/example-refzdr.csv", 0, isrr=3, mask=1, acc='H')
-eee = mt.readcsv(f"{path}/example-kdpzdr.csv", 0, isrr=3, mask=1, acc='H')
+aaa = mt.readcsv(f"{path}/example-res.csv", 0, isrr=3, mask=1, acc='H')
+bbb = mt.readcsv(f"{path}/example-cnn.csv", 0, isrr=3, mask=1, acc='H')
+ccc = mt.readcsv(f"{path}/example-ref.csv", 0, isrr=3, mask=1, acc='H')
+ddd = mt.readcsv(f"{path}/example-kdp.csv", 0, isrr=3, mask=1, acc='H')
+eee = mt.readcsv(f"{path}/example-refzdr.csv", 0, isrr=3, mask=1, acc='H')
+fff = mt.readcsv(f"{path}/example-kdpzdr.csv", 0, isrr=3, mask=1, acc='H')
 zzz = mt.readcsv(f"/home/zry/code/gauge_all.csv", 0, isrr=0, mask=1)
 
-titles = ['CNN Model', 'R(Z)', 'R(kdp)', 'R(kdp,zdr)']
+titles = ['Res Model', 'CNN Model', 'R(Z)', 'R(kdp)', 'R(Z,zdr)', 'R(kdp,zdr)']
 
 '''match'''
 _, _, index, columns = mt.match_df(aaa, zzz)
 aaa = aaa.loc[index, columns]
 bbb = bbb.loc[index, columns]
 ccc = ccc.loc[index, columns]
-# ddd = ddd.loc[index, columns]
+ddd = ddd.loc[index, columns]
 eee = eee.loc[index, columns]
+fff = fff.loc[index, columns]
 zzz = zzz.loc[index, columns]
 
-date = ''
-# date = '20190809'
-# aaa = aaa.loc[date]
-# bbb = bbb.loc[date]
-# ccc = ccc.loc[date]
-# # ddd = ddd.loc[date]
-# eee = eee.loc[date]
-# zzz = zzz.loc[date]
+
 
 
 
 '''scatters'''
-def plot_all_scatters(aaa, bbb, ccc, eee, zzz,
-                      fig_path, met_path):
-    flg = (zzz>=0.1) & (aaa>=0.1) & (bbb>=0.1) & (ccc>=0.1) & (eee>=0.1)
+def plot_all_scatters(aaa, bbb, ccc, ddd, eee, fff, zzz,
+                      fig_path, met_path, box_path):
+    loc = (zzz>=0.1) & (aaa>=0.1) & (bbb>=0.1) & (ccc>=0.1) & (eee>=0.1) & (fff>=0.1) & (ddd>=0.1)
     
-    aaa = aaa.values.flatten()
-    bbb = bbb.values.flatten()
-    ccc = ccc.values.flatten()
-    # ddd = ddd.values.flatten()
-    eee = eee.values.flatten()
-    zzz = zzz.values.flatten()
-    loc = (zzz>=0.1) & (aaa>=0.1) & (bbb>=0.1) & (ccc>=0.1) & (eee>=0.1)
-    # print(len(np.where(loc==True)[0]))
+    aaa = aaa[loc].values.flatten()
+    bbb = bbb[loc].values.flatten()
+    ccc = ccc[loc].values.flatten()
+    ddd = ddd[loc].values.flatten()
+    eee = eee[loc].values.flatten()
+    fff = fff[loc].values.flatten()
+    zzz = zzz[loc].values.flatten()
     
         
-    # 创建一个包含3个子图的图形
-    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-    # plt.subplots_adjust(hspace=0.5)
-    # plt.subplots_adjust(wspace=0.5)
-    plt.subplots_adjust(left=0, right=1.2, top=1, bottom=0)
+    # # 创建一个包含3个子图的图形
+    # fig, axs = plt.subplots(3, 2, figsize=(18, 10))
+    # # plt.subplots_adjust(hspace=0.5)
+    # # plt.subplots_adjust(wspace=0.5)
+    # plt.subplots_adjust(left=0, right=0.8, top=1.2, bottom=0)
     
-    hd1 = mt.hist2d(axs[0][0], zzz[loc], aaa[loc], bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
-    hd2 = mt.hist2d(axs[0][1], zzz[loc], bbb[loc], bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
-    hd3 = mt.hist2d(axs[1][0], zzz[loc], ccc[loc], bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
-    # hd4 = mt.hist2d(axs[1][1], zzz[loc], ddd[loc], bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
-    hd5 = mt.hist2d(axs[1][1], zzz[loc], eee[loc], bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
+    # hd1 = mt.plot_hist2d(axs[0][0], zzz, aaa, bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
+    # hd2 = mt.plot_hist2d(axs[0][1], zzz, bbb, bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
+    # hd3 = mt.plot_hist2d(axs[1][0], zzz, ccc, bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
+    # hd4 = mt.plot_hist2d(axs[1][1], zzz, ddd, bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
+    # hd5 = mt.plot_hist2d(axs[2][0], zzz, eee, bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
+    # hd6 = mt.plot_hist2d(axs[2][1], zzz, fff, bins=[np.arange(101)]*2, drawline=1, labels=['Gauge (mm)', 'Radar (mm)'], equal=1, showmet=1)
     
-    '''cb'''
-    fig.colorbar(hd1[3], ax=axs, location='right', pad=0.05)
-    '''title'''
-    count = 0
-    for i in range(2):
-        for j in range(2):
-            axs[i][j].set_title(titles[count])
-            count += 1
-    '''save'''
-    fig.savefig(fig_path, transparent=0, dpi=600, bbox_inches='tight')
+    # '''cb'''
+    # fig.colorbar(hd1[3], ax=axs, location='right', pad=0.05)
+    # '''title'''
+    # count = 0
+    # for i in range(3):
+    #     for j in range(2):
+    #         axs[i][j].set_title(titles[count])
+    #         count += 1
+    # '''save'''
+    # fig.savefig(fig_path, transparent=0, dpi=600, bbox_inches='tight')
+
+    # '''boxplot'''
+    # mt.plot_boxplot(zzz,[aaa,bbb,ccc,ddd,eee,fff],titles,[0,10,20,30,40,50,100],box_path)
+
     
     '''metrics'''
-    met = {**mt.get_metrics(zzz[loc], aaa[loc]), **mt.get_metrics_hit(zzz,aaa,4)}
+    met =                {**mt.get_metrics(zzz, aaa)}
     met = pd.DataFrame(met, index=[titles[0]], columns=met.keys())
-    met.loc[titles[1]] = {**mt.get_metrics(zzz[loc], bbb[loc]), **mt.get_metrics_hit(zzz,bbb,4)}
-    met.loc[titles[2]] = {**mt.get_metrics(zzz[loc], ccc[loc]), **mt.get_metrics_hit(zzz,ccc,4)}
-    # met.loc[titles[3]] = mt.get_metrics(zzz[loc], ddd[loc])
-    met.loc[titles[3]] = {**mt.get_metrics(zzz[loc], eee[loc]), **mt.get_metrics_hit(zzz,eee,4)}
-    met = met[['RMB', 'MAE', 'RMSE', 'CC', 'POD', 'FAR', 'CSI']]
-    met.to_csv(met_path)
-    print(met)
+    met.loc[titles[1]] = {**mt.get_metrics(zzz, bbb)}
+    met.loc[titles[2]] = {**mt.get_metrics(zzz, ccc)}
+    met.loc[titles[3]] = {**mt.get_metrics(zzz, ddd)}
+    met.loc[titles[4]] = {**mt.get_metrics(zzz, eee)}
+    met.loc[titles[5]] = {**mt.get_metrics(zzz, fff)}
+    met = met[['BIAS', 'MAE', 'RMSE', 'CC']]
+    # met.to_csv(met_path)
+    # print(met)
+    return met
 
-# plot_all_scatters(aaa, bbb, ccc, eee, zzz, f"{path}/example-hour-{date}.png", f"{path}/example-hour-{date}.csv")
-mt.boxplot(zzz,aaa,bbb,ccc,eee,titles,f"{path}/example-hour-box-{date}.png")
+
+
+# plot_all_scatters(aaa, bbb, ccc, ddd,eee,fff, zzz, f"{path}/example-hour.png", f"{path}/example-hour.csv", f"{path}/example-hour-box.png")
+date = '20190729'
+met = pd.DataFrame(columns=['BIAS', 'MAE', 'RMSE', 'CC'])
+cases = [20190517, 20190722, 20190729, 20190804, 20190806, 20190809, 20190909, 20190912]
+zzzmeans = []
+for date in cases:
+    date = str(date)
+    aaa1 = aaa.loc[date]
+    bbb1 = bbb.loc[date]
+    ccc1 = ccc.loc[date]
+    ddd1 = ddd.loc[date]
+    eee1 = eee.loc[date]
+    fff1 = fff.loc[date]
+    zzz1 = zzz.loc[date]
+    met1 = plot_all_scatters(aaa1, bbb1, ccc1, ddd1,eee1,fff1, zzz1, f"{path}/example-hour-{date}.png", f"{path}/example-hour-{date}.csv", f"{path}/example-hour-box-{date}.png")
+    met = pd.concat((met, met1))
+    zzzmeans += [zzz.loc[date].sum(axis=0).values.flatten().mean()]
+    print(date, 'done')
+print(met)
+fig, axs = plt.subplots(4,1,figsize=(12,10))
+for i in range(len(titles)):
+    method = titles[i]
+    line_style = '-' if 'Model' in method else '--'
+    axs[0].plot(np.arange(len(cases)), met.loc[method, 'BIAS'], label=method, linestyle=line_style)
+    axs[1].plot(np.arange(len(cases)), met.loc[method, 'MAE'], label=method, linestyle=line_style)
+    axs[2].plot(np.arange(len(cases)), met.loc[method, 'RMSE'], label=method, linestyle=line_style)
+    axs[3].plot(np.arange(len(cases)), zzzmeans)
+for i in range(3):
+    axs[i].legend()  # 显示图例
+    axs[i].grid(True)  # 显示网格
+plt.savefig(f"{path}/example-hour-cases.png")
 
 
 '''distribution'''
--- eval-p2r.py
+++ eval-p2r.py
@@ -1,25 +1,16 @@
-import sys
-sys.path.append('my')
 import my.utils.point2radar as p2r
 import os
 
 siteinfo_path = '/home/zry/code/gauge2bjxsy_145.csv'
-path = '/data/zry/radar/Xradar_npy_qpe/BJXSY-test/ResQPE-3399-1-vlr-wmse'
-savepath = '/home/zry/code/QPE_prv_ppi/model/based_on_202407/ResQPE-3399-1-vlr-wmse/eval'
+path = '/data/zry/radar/Xradar_npy_qpe/run2019'
+savepath = '/home/zry/code/QPE_prv_ppi/dataset/20240909eval_2models'
 if not os.path.exists(savepath):
     os.makedirs(savepath)
 
-df = p2r.lookup_site2radar(siteinfo_path, path, temp_var=0)
+df,df1,df2,df3,df4,df5 = p2r.lookup_site2radar(siteinfo_path, path)
 df.to_csv(f'{savepath}/example-ref.csv')
-
-df = p2r.lookup_site2radar(siteinfo_path, path, temp_var=1)
-df.to_csv(f'{savepath}/example-kdp.csv')
-
-df = p2r.lookup_site2radar(siteinfo_path, path, temp_var=2)
-df.to_csv(f'{savepath}/example-refzdr.csv')
-
-df = p2r.lookup_site2radar(siteinfo_path, path, temp_var=3)
-df.to_csv(f'{savepath}/example-kdpzdr.csv')
-
-df = p2r.lookup_site2radar(siteinfo_path, path, temp_var=4)
-df.to_csv(f'{savepath}/example-dl.csv')
+df1.to_csv(f'{savepath}/example-kdp.csv')
+df2.to_csv(f'{savepath}/example-refzdr.csv')
+df3.to_csv(f'{savepath}/example-kdpzdr.csv')
+df4.to_csv(f'{savepath}/example-res.csv')
+df5.to_csv(f'{savepath}/example-cnn.csv')
